1af52ffa7d1a4eb420018ee30a412bbc6b694d16
Irina Molodtsova <irinamolodtsova98@gmail.com>
2025-06-21 13:17:23 +0300

    diploma

--- src/test/java/mobile_tests/tests/TestBase.java
+++ src/test/java/mobile_tests/tests/TestBase.java
@@ -1,11 +1,13 @@
 package mobile_tests.tests;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
 import mobile_tests.drivers.BrowserStackDriver;
 import mobile_tests.drivers.EmulateMobileDriver;
 import mobile_tests.drivers.RealMobileDriver;
+import mobile_tests.helpers.Attach;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -39,5 +41,9 @@ public class TestBase {
     @AfterEach
     void afterEach() {
         closeWebDriver();
+        String sessionId = Selenide.sessionId().toString();
+        Attach.pageSource();
+        closeWebDriver();
+        Attach.addVideo(sessionId);
     }
 }
